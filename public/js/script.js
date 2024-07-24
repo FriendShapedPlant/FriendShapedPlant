e4e4eb749ad834fb22b3d957b2de428635699270
Changeling <125620617+FriendShapedPlant@users.noreply.github.com>
2024-07-24 13:05:13 -0400

    Update script.js
    Navbar Links target self (all other links set to target blank in header)

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', (event) =>{
-	
+			
 		//Navbar
 		
 		navbar.innerHTML = `
@@ -11,6 +11,12 @@ window.addEventListener('load', (event) =>{
 			<li><a href="GhostZapper.html">Ghost Zapper</a></li>
 		</ul>
 		`;
+		
+		//Navbar links all target self
+		var i_will_target_self = document.querySelectorAll("#navbar a");
+		for (var i = 0; i < 5; i++) {
+			i_will_target_self[i].target = "_self";
+		}
 		
 		//Footer
 		footer.innerHTML = '<a href="https://neocities.org/site/friendshapedplant">Site Profile</a> | <a href="https://github.com/FriendShapedPlant/FriendShapedPlant">Github Repo</a>';
