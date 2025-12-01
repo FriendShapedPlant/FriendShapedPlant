47262bc030a401c4523051d2c3bff8b0e0a8141a
Changeling <125620617+FriendShapedPlant@users.noreply.github.com>
2025-12-01 15:07:55 -0500

    the NEW freezeframe.js

--- js/freezeframe.js
+++ js/freezeframe.js
@@ -1,6 +1,111 @@
-/*Freezeframe.js play/pause buttons (and disable funny sizing and hover)*/
-document.addEventListener("DOMContentLoaded", function(event) {
-  const e = new Freezeframe({ trigger: false, responsive: false });
-  document.getElementById("play-gif").addEventListener("click", function(){ e.start() });
-  document.getElementById("stop-gif").addEventListener("click", function(){ e.stop() });
+class FreezeImages {
+  constructor(options = {}) {
+    // Set default params
+    this.selector = options.selector || ".freeze"
+    this.imgCls = "ff-img";
+    this.canvasCls = "ff-canvas";
+    this.hover = (options.hover === true) ? true : false;
+
+    // Finds all images with selector class and within elements with the selected class
+    //  and creates list
+    const imgList = document.querySelectorAll(`img${this.selector}, ${this.selector} img`);
+    this.imgList = imgList;
+
+    // Creates <style> tag for new elements
+    if (!this.noCSS) {
+      const style = document.createElement('style');
+      style.textContent = `
+        .ff-container {
+          display: inline-block;
+          position: relative;
+        }
+
+        .ff-container.ff-hover:hover .ff-active {
+          position: absolute;
+          opacity: 0;
+        }
+
+        .ff-container.ff-hover:hover .ff-inactive {
+          position: static;
+          opacity: 1;
+        }
+
+        .ff-inactive {
+          position: absolute;
+          opacity: 0;
+        }
+      `;
+      document.head.appendChild(style);
+    }
+
+    // Loops through all images
+    for (const img of this.imgList) {
+      // Gives <img> the inactive class, which hides GIF by default
+      img.className = `${this.imgCls} ff-inactive`;
+
+      // Creates <canvas> of GIF and copies data of first frame of animation
+      let canvas = document.createElement("canvas");
+      canvas.width = img.width;
+      canvas.height = img.height;
+      canvas.className = `${this.canvasCls} ff-active`;
+      canvas.getContext('2d').imageSmoothingEnabled = false;
+      canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height);
+
+
+      // Creates container that will hold both <img> and <canvas>
+      let wrapper = document.createElement("div");
+      wrapper.className = "ff-container";
+      if (this.hover) wrapper.classList.add("ff-hover");
+
+      // Inserts container with <img> and <canvas> where <img> originally was
+      img.parentNode.insertBefore(wrapper, img);
+      wrapper.appendChild(img);
+      wrapper.appendChild(canvas);
+    }
+  }
+
+  start() { // Starts animations by switching class names
+    for (const img of this.imgList) {
+      img.className = `${this.imgCls} ff-active`;
+      img.nextSibling.className = `${this.canvasCls} ff-inactive`;
+    }
+  }
+
+  stop() { // Stops animations by switching class names
+    for (const img of this.imgList) {
+      img.className = `${this.imgCls} ff-inactive`;
+      img.nextSibling.className = `${this.canvasCls} ff-active`;
+    }
+  }
+
+  toggle() { // Toggles animations by switching class names based on current state
+    for (const img of this.imgList) {
+      let imgNewCls = (img.className.includes('ff-inactive')) ? "ff-active": "ff-inactive";
+      let canvasNewCls = (img.className.includes('ff-inactive')) ? "ff-inactive": "ff-active";
+
+      img.className = `${this.imgCls} ${imgNewCls}`;
+      img.nextSibling.className = `${this.canvasCls} ${canvasNewCls}`;
+    }
+  }
+}
+
+// Waits for page to finish loading
+document.addEventListener("readystatechange", function () {
+  if (document.readyState === "complete") {
+    // Initialize script
+    const f = new FreezeImages ({noCss: true})
+
+    // Set event listeners for all buttons
+    for(const el of document.getElementsByClassName('play-gif')) {
+      el.addEventListener('click', () => f.start());
+    }
+
+    for(const el of document.getElementsByClassName('stop-gif')) {
+      el.addEventListener('click', () => f.stop());
+    }
+
+    for(const el of document.getElementsByClassName('toggle-gif')) {
+      el.addEventListener('click', () => f.toggle());
+    }
+  }
 });
